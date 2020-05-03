53b46a25401bd646ffcccc65edfd834beea09ffd
Khalimudin <khalimudin@gmail.com>
2020-05-04 06:24:10 +0700

    client menu

--- tickets/views.py
+++ tickets/views.py
@@ -5,3 +5,7 @@ from django.shortcuts import render
 class WelcomeView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'welcome.html')
+
+
+def menu(request):
+    return render(request, 'menu.html')
